5a8c97bcc28c2935bab31a372ad4867660d322b7
unknown <shiva1894300@gmail.com>
2021-01-28 19:12:04 +0530

    connecting mongodb

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -50,17 +50,21 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: "fail",
-      data: "Invalid data sent!",
+      message: err,
     });
   }
 };
 
 exports.updateTour = async (req, res) => {
   try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
     res.status(200).json({
       status: "success",
       data: {
-        tours: "<Updated tour here...>",
+        tour,
       },
     });
   } catch (err) {
@@ -71,9 +75,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
--- models/tourModels.js
+++ models/tourModels.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
 
 const tourSchema = new mongoose.Schema({
-  name: { type: String, required: [true, "A tour must have a name"], unique: true },
-  rating: { type: Number, default: 4.5 },
+  name: { type: String, required: [true, "A tour must have a name"], unique: true, trim: true },
+  duration: { type: String, required: [true, "A tour must have a duration"] },
+  maxGroupSize: { type: Number, required: [true, "A tour must have a Group Size"] },
+  difficulty: { type: String, required: [true, "A tour must have a difficulty"] },
+  ratingsAverage: { type: Number, default: 4.5 },
+  ratingsQuantity: { type: Number, default: 0 },
   price: { type: Number, required: [true, "A tour must have a price"] },
+  priceDiscount: Number,
+  summary: { type: String, trim: true, required: [true, "A tour must have a description"] },
+  description: { type: String, trim: true },
+  imageCover: { type: String, required: [true, "A tour must have a cover image"] },
+  images: [String],
+  createdAt: { type: Date, default: Date.now() },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model("Tour", tourSchema); // inside the model name will create the collection name+s in db
 
 module.exports = Tour;
-
-
-
 
 // const testTour = new Tour({
 //     name: "The Forest jack",
