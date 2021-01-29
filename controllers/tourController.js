efd22d1647b66addc93dc0a9753e68ba07b96cfb
unknown <shiva1894300@gmail.com>
2021-01-29 17:08:11 +0530

    adding features in api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 import { Tour } from "../models/tourModels";
+import APIFeatures from "../utils/apiFeatures";
 
 export const aliasTopTours = async (req, res, next) => {
   req.query.limit = "5";
@@ -9,69 +10,22 @@ export const aliasTopTours = async (req, res, next) => {
 
 export const getAllTours = async (req, res) => {
   try {
-    //BUILD QUERY
-    //1a) filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
+    // EXECUTE QUERY
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
-    //1b)Advanced filtering
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" "); // price,ratingsAverage => sort("price ratingsAverage")
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    //3) Field limiting
-    if (req.query.fields) {
-      console.log(req.query);
-      const fields = req.query.fields.split(",").join(" "); // name,duration,price => select("name duration price")
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error("This page does not exist");
-    }
-
-    //EXECUTE QUERY
-    const tours = await query;
-
-    //SEND RESPONSE
+    // SEND RESPONSE
     res.status(200).json({
       status: "success",
-      result: tours.length,
+      results: tours.length,
       data: {
         tours,
       },
     });
-
-    // const tours = await Tour.find();
-
-    // req.query={}
-    // const tours = await Tour.find({
-    //   duration: 5,
-    //   difficulty: "easy",
-    // });
-    //MONGOOOSE METHOD
-    // const query = await Tour.find().where("duration").equals(5).where("difficulty").equals("easy");
   } catch (err) {
     res.status(404).json({
       status: "fail",
