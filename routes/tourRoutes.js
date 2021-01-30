cdb2db0315ab9e523a8a72daeb6197ef8f572566
unknown <shiva1894300@gmail.com>
2021-01-30 22:11:29 +0530

    adding all middlewares

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,8 @@ import {
   getTour,
   updateTour,
   deleteTour,
+  getTourStats,
+  getMonthlyPlan,
 } from "../controllers/tourController";
 
 const router = express.Router();
@@ -13,6 +15,9 @@ const router = express.Router();
 // router.param("id", tourController.checkID);
 
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
+
+router.route("/tour-stats").get(getTourStats);
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 
 router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
