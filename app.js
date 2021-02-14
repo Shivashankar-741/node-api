7bb432bc8ecd56af8f4ac94a86b2527bef1ff14e
Shivashankar-741 <shiva1894300@gmail.com>
2021-02-14 21:43:36 +0530

    error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 
+import AppError from "./utils/appError.js";
+import globalErrorHandler from "./controllers/errorController";
 import tourRouter from "./routes/tourRoutes";
 import userRouter from "./routes/userRoutes";
 
@@ -32,6 +34,23 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  // res.statusCode(404).json({
+  //   status: "pass",
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = "fail";
+  // err.statusCode = 404;
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  //14/2/21 => NOTE:when the error occurs here it will break the middle of the middleware and directly goes globalErrorHandler
+});
+
+//ERROR HANDLING MIDDLEWARE
+app.use(globalErrorHandler);
 
 // 4)START SERVER
 
--- server.js
+++ server.js
@@ -22,4 +22,4 @@ const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`.yellow.bold);
-});
+}); 
